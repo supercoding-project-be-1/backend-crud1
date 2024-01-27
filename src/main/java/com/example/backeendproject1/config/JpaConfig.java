2b84e7fecd8e3eb30535cad8b3ce270500e128e4
hunhee kim <kshkhh4015@gmail.com>
2024-01-27 20:05:34 +0900

    # comment 병합 에러 해결

--- src/main/java/com/example/backeendproject1/config/JpaConfig.java
+++ src/main/java/com/example/backeendproject1/config/JpaConfig.java
@@ -7,7 +7,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -15,7 +14,9 @@ import java.util.Map;
 
 @Configuration
 @EnableJpaRepositories(
-        basePackages = {"com.example.backeendproject1.repository.comment", "com.example.backeendproject1.repository.post", "com.example.backeendproject1.repository.member"},
+        basePackages = {"com.example.backeendproject1.repository.comment",
+                        "com.example.backeendproject1.repository.post",
+                        "com.example.backeendproject1.repository.member"},
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef =  "tmJpa"
 )
@@ -23,10 +24,10 @@ public class JpaConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("8282");
+        dataSource.setUsername("ajy8u9cn1h4pohh9e4pi");
+        dataSource.setPassword("pscale_pw_mMH67dgtpoKJXw6aom9s7ZadYESfbPVXbpzzTbAX3OV");
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/project_1?useUnicode=true&characterEncoding=UTF-8");
+        dataSource.setUrl("jdbc:mysql://gcp.connect.psdb.cloud:3306/hunsd");
         return dataSource;
     }
 
@@ -35,11 +36,12 @@ public class JpaConfig {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
         em.setPackagesToScan("com.example.backeendproject1.repository.comment", "com.example.backeendproject1.repository.post", "com.example.backeendproject1.repository.member");
-                JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
 
         Map<String, Object> properties = new HashMap<>();
-     //   properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         properties.put("hibernate.format_sql", "true");
         properties.put("hibernate.use_sql_comment", "true");
 
@@ -48,8 +50,9 @@ public class JpaConfig {
         return em;
     }
 
+
     @Bean(name = "tmJpa")
-    public PlatformTransactionManager transactionManger(DataSource dataSource) {
+    public JpaTransactionManager transactionManger(DataSource dataSource) {
 
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory(dataSource).getObject());
--- src/main/java/com/example/backeendproject1/config/WebConfig.java
+++ src/main/java/com/example/backeendproject1/config/WebConfig.java
@@ -5,6 +5,8 @@
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
