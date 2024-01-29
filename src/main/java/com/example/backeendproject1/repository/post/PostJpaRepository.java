5f1ff6ddf2bfc66835e8959ee0831489df5cbad1
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-01-30 07:16:09 +0900

    Delete src/main/java/com/example/backeendproject1/repository/post directory

--- src/main/java/com/example/backeendproject1/repository/post/PostJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backeendproject1.repository.post;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostJpaRepository extends JpaRepository<PostEntity, Integer> {
-}
