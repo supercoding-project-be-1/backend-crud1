634f7d86fe8bac14f116def7c5de9fc0157818e8
gnsgmls <145729081+gnsgmls@users.noreply.github.com>
2024-01-30 07:30:24 +0900

    Add files via upload

---
+++ src/main/java/com/example/backeendproject1/repository/post/PostEntity.java
@@ -0,0 +1,51 @@
+package com.example.backeendproject1.repository.post;
+
+import com.example.backeendproject1.web.dto.PostBody;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="posts")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //author
+//    @ManyToOne
+//    @JoinColumn(name="email")
+//    private Member member;
+//    //mappedby해주기
+    @Column(name = "author")
+    private String author;
+
+    //nickname
+    //nickname은 member, comment 랑도 연결해야 하는데..
+//    @ManyToOne
+//    @JoinColumn(name="nickname")
+//    private Member member;
+    //    //mappedby해주기
+
+
+    @Column(name = "title")
+    private String title;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public void setPostBody(PostBody postBody) {
+        this.author = postBody.getAuthor();
+        this.title = postBody.getTitle();
+        this.content = postBody.getContent();
+    }
+}
---
+++ src/main/java/com/example/backeendproject1/repository/post/PostJpaRepository.java
@@ -0,0 +1,7 @@
+package com.example.backeendproject1.repository.post;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostJpaRepository extends JpaRepository<PostEntity, Integer> {
+}
